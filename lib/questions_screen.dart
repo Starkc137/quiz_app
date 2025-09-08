188f7cbf030e38ef1236923d6d4ea3703b039f45
nhlakanipho <ndlanzi003@gmail.com>
2025-09-08 15:04:57 +0200

    add questions screen

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,8 +12,38 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreen extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      currentQuestionIndex ++;
+    });
+  }
+
   @override
   Widget build(context) {
-    return Text('QuestionsScreen');
+    final currentQuestion = questions[currentQuestionIndex];
+
+    return Container(
+      margin: EdgeInsets.all(40),
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text, 
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+              ),
+            const SizedBox(height: 30),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(answerText: answer, onTap: answerQuestion);
+            }),
+          ],
+        ),
+      ),
+    );
   }
 }
